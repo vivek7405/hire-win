518d1330da8c60f8619b2c9e20963bf24f20d127
Vivek <vivek7405@gmail.com>
2022-06-13 21:16:36 +0530

    Active, Expired & Archived radio

--- app/core/components/RadioGroupField.tsx
+++ app/core/components/RadioGroupField.tsx
@@ -7,14 +7,18 @@ export interface RadioGroupFieldProps extends PropsWithoutRef<JSX.IntrinsicEleme
   /** Field name. */
   name: string
   /** Field label. */
-  label: string
+  label?: string
   outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
   testid?: string
   options: any
+  isBorder?: boolean
+  isCol?: boolean
+  onChange?: any
+  initialValue?: string
 }
 
 export const RadioGroupField = forwardRef<HTMLInputElement, RadioGroupFieldProps>(
-  ({ label, outerProps, name, options, ...props }, ref) => {
+  ({ label, outerProps, name, options, isBorder, isCol, initialValue, ...props }, ref) => {
     const {
       register,
       control,
@@ -38,24 +42,32 @@ export const RadioGroupField = forwardRef<HTMLInputElement, RadioGroupFieldProps
           const { onChange, value } = field
 
           return (
-            <div className="flex flex-col space-y-2">
-              <label
-                data-testid={`${props.testid && `${props.testid}-`}label`}
-                className="block text-sm font-medium text-gray-700"
-              >
-                {label}
-              </label>
+            <div className={`flex flex-col space-y-2`}>
+              {label && (
+                <label
+                  data-testid={`${props.testid && `${props.testid}-`}label`}
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {label}
+                </label>
+              )}
 
               <RadioGroup.Root
-                value={value}
+                value={value || initialValue}
                 onValueChange={(e) => {
+                  props.onChange && props.onChange(e)
                   onChange(e)
                 }}
-                className="flex flex-col space-y-2"
+                className={`flex ${isCol ? "flex-col space-y-2" : ""}`}
               >
                 {options?.map((option, i) => {
                   return (
-                    <div key={i} className="w-full border p-2 rounded flex flex-row items-center">
+                    <div
+                      key={i}
+                      className={`w-full p-2 ${
+                        isBorder ? "border" : ""
+                      } flex flex-row items-center`}
+                    >
                       <RadioGroup.Item className="relative w-full" value={option}>
                         <RadioGroup.Indicator
                           className={`
@@ -68,7 +80,18 @@ export const RadioGroupField = forwardRef<HTMLInputElement, RadioGroupFieldProps
                             after:bg-theme-500 after:block after:w-2 after:h-2 after:rounded-full
                           `}
                         />
-                        <span className="pl-8">{option}</span>
+                        <span
+                          className={`
+                            absolute
+                            border
+                            border-gray-300
+                            rounded-full
+                            p-1
+                            top-1
+                            after:block after:w-2 after:h-2 after:rounded-full
+                          `}
+                        />
+                        <span className="pl-6">{option}</span>
                       </RadioGroup.Item>
                     </div>
                   )
