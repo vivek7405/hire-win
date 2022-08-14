5db88ced5af3e087a8eb55a98cd7ea16068a52a4
Vivek <vivek7405@gmail.com>
2022-08-14 13:02:58 -0400

    Revert "Added console logs for debugging infinite while loop in scheduleToTakenSlots"
    This reverts commit bd911c060d45d25fc851911611590ac7a52f9c5c.

--- app/scheduling/interviews/utils/scheduleToTakenSlots.ts
+++ app/scheduling/interviews/utils/scheduleToTakenSlots.ts
@@ -41,8 +41,6 @@ export function scheduleToTakenSlots(
   between: TimeSlot,
   timezone: string
 ): TimeSlot[] {
-  console.log("Now inside scheduleToTakenSlots...")
-
   if (Object.keys(schedule).length === 0) {
     return [between]
   }
@@ -50,26 +48,13 @@ export function scheduleToTakenSlots(
   const result: TimeSlot[] = []
   let cursor = between.start
 
-  console.log("cursor:")
-  console.log(cursor)
-  const betweenEndPlusOne = addDays(between.end, 1)
-  console.log("betweenEndPlusOne:")
-  console.log(betweenEndPlusOne)
-  console.log("Entering while (cursor <= betweenEndPlusOne)...")
-  while (cursor <= betweenEndPlusOne) {
+  while (cursor <= addDays(between.end, 1)) {
     const slot: TimeSlot = {
       start: endOfLastWorkDayBefore(cursor, schedule, timezone),
       end: startOfFirstWorkDayOnOrAfter(cursor, schedule, timezone),
     }
     result.push(slot)
     cursor = addDays(slot.end, 1)
-
-    console.log("cursor:")
-    console.log(cursor)
   }
-  console.log("Exited while (cursor <= betweenEndPlusOne)")
-  console.log("result:")
-  console.log(result)
-
   return result
 }
