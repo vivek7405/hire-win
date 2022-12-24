dbda3af513c9319eddf1c661c9612e29dfff831f
Vivek <vivek7405@gmail.com>
2022-12-24 15:08:10 +0530

    Updated app sumo listing url

--- src/plans/components/UpgradeMessage.tsx
+++ src/plans/components/UpgradeMessage.tsx
@@ -4,7 +4,7 @@ type UpgradeMessagePropsType = {
 const UpgradeMessage = ({ message }: UpgradeMessagePropsType) => {
   return (
     <a
-      href=""
+      href="https://appsumo.com/products/hirewin"
       className="px-4 py-1 rounded-lg border bg-theme-50 border-theme-600 flex items-center space-x-2 text-theme-800"
     >
       <span className="text-3xl">👉</span>
