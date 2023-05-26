32238a98b6abee9c9448631009cceaeb627586ad
Vivek <vivek7405@gmail.com>
2023-05-26 15:54:06 +0530

    Added Google Tag Manager (GTM) scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 class MyDocument extends Document {
   // Only uncomment if you need to customize this behaviour
@@ -11,6 +12,14 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <Script strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MGB4XPJ');`}
+          </Script>
+
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-CL7G43RP1E" />
 
           <script
@@ -25,6 +34,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MGB4XPJ"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
